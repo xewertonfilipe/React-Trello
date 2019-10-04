d87ea581d724ffbee88141129d90e1c2994890ac
Ewerton Filipe <e.nascimento.pereira@avanade.com>
2019-10-04 00:20:54 -0300

    toggleComponent

--- src/components/Filtro/index.js
+++ src/components/Filtro/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import FiltroAvancado from '../FiltroAvancado';
 
 import { Container } from './style';
 
-export default function Filtro() {
-  return (
-    <Container>
-      <input type="text" placeholder="Pesquisar" />
-      <button type="button" onClick={() => this.operation()}>
-        Filtro Avançado
-      </button>
-    </Container>
-  );
+class Filtro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+    this.toggleComponent = this.toggleComponent.bind(this);
+  }
+
+  toggleComponent = () => {
+    const { show } = this.state;
+    this.setState({ show: !show });
+  };
+
+  render() {
+    return (
+      <Container>
+        <input type="text" placeholder="Pesquisar" />
+        <button type="button" onClick={this.toggleComponent}>
+          Filtro Avançado
+        </button>
+        {this.state.show && <FiltroAvancado />}
+      </Container>
+    );
+  }
 }
+
+export default Filtro;
